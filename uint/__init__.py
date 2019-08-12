e28ab3a38e907e2e791a49a269df6308a470cff6
Takumi Sueda <puhitaku@gmail.com>
2019-08-12 23:34:38 +0900

    Get signed value for Int.raw

--- uint/__init__.py
+++ uint/__init__.py
@@ -164,6 +164,20 @@ class Int(Uint):
         self.signed = True
         self.me = 'int'
 
+    def is_negative(self):
+        return super().raw & (1 << (self.bits - 1))
+
+    @property
+    def raw(self):
+        if self.is_negative():
+            return ~super().raw + 1
+        else:
+            return super().raw
+
+    @raw.setter
+    def raw(self, value):
+        self._raw = value & self.mask
+
     def __repr__(self):
         return f'<int{self.bits}, value={self.raw}>'
 
