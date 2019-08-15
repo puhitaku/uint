d609df6699f7539fa3d950432dcb2f6da0d305e9
Takumi Sueda <puhitaku@gmail.com>
2019-08-16 01:58:05 +0900

    Int: handle minus values / output raw and native

--- uint/int.py
+++ uint/int.py
@@ -7,27 +7,41 @@ from uint import Uint
 class Int(Uint):
     def __init__(self, value: int, bits: int):
         super().__init__(value, bits)
+
+        # we replace self.raw as Uint's __init__ doesn't accept negative value
+        self.raw = value
+
         self.signed = True
         self.me = 'int'
 
-    def is_negative(self):
-        return super().raw & (1 << (self.bits - 1))
-
     @property
     def raw(self):
-        if self.is_negative():
-            return ~super().raw + 1
-        else:
-            return super().raw
+        return self._raw
 
     @raw.setter
     def raw(self, value):
-        self._raw = value & self.mask
+        if self.is_twocomp(value):
+            self._raw = -self.twocomp(value & self.mask)
+        else:
+            self._raw = value & self.mask
 
-    def __repr__(self):
+    @property
+    def native(self):
+        if self._raw < 0:
+            return self.twocomp(self._raw)
+        else:
+            return self._raw
+
+    def __str__(self) -> str:
         return f'<int{self.bits}, value={self.raw}>'
 
     def __neg__(self):
         return self._calc_alone(operator.inv) + 1
 
+    def twocomp(self, v):
+        if v < 0:
+            v = -v
+        return self.mask - v + 1
 
+    def is_twocomp(self, v):
+        return bool(v & (1 << (self.bits - 1)))
