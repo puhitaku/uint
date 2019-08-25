bac87b225592b9b11636bd512e655cb922599d32
Takumi Sueda <puhitaku@gmail.com>
2019-08-26 01:31:35 +0900

    Add Int tests

---
+++ uint/test_int.py
@@ -0,0 +1,19 @@
+from uint import Uint, Int
+
+
+def test_and():
+    x, y = Uint(0xffff, 16), Int(0x805a, 16)
+    assert (x & y).raw == 32858
+    assert (y & x).raw == -32678
+    assert (x & y).native == 0x805a
+    assert (y & x).native == 0x805a
+
+
+def test_neg():
+    x = Int(0x01, 8)
+    assert x.raw == 1
+    assert x.native == 0x01
+
+    x = -x
+    assert x.raw == -1
+    assert x.native == 0xff
